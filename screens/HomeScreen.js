261992d5a8cb594059065365c80bafecbc058f64
Tommy Wong <lokhim128@gmail.com>
2023-01-05 11:07:09 +0800

    Init

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return ( 
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+   );
+}
+
+export default HomeScreen;
---
+++ themes/Color.js
@@ -0,0 +1,17 @@
+const Primary = '#4C56EE';
+const PrimaryLight = '#9190FF';
+const PrimaryDark = '#1639AA';
+const Secondary = '#F88904';
+const SecondaryLight = '#FFBA53';
+const SecondaryDark = '#A55D00';
+const Dark = '#393939';
+
+const Color = {
+  Primary,
+  PrimaryLight,
+  PrimaryDark,
+  Secondary,
+  SecondaryLight,
+  SecondaryDark,
+  Dark
+};
