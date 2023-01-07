678502528f30a9d1f6690221eb1f43efbedec325
Tommy Wong <lokhim128@gmail.com>
2023-01-07 16:26:10 +0800

    new file:   components/InputField.js 	modified:   screens/AddScreen.js

---
+++ components/InputField.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { FontAwesome, AntDesign, Fontisto, EvilIcons } from '@expo/vector-icons';
+
+import Color from '../themes/Color.js';
+
+const InputField = (props) => {
+
+  return ( 
+    <TextInput 
+      style={styles.inputField}
+      placeholder={props.placeholder}
+    />
+   );
+}
+
+const styles = StyleSheet.create({
+  inputField: {
+    width: '100%',
+    height: 30,
+    borderBottomWidth: 1.5,
+    borderColor: '#fff',
+  },
+
+
+});
+
+export default InputField;
