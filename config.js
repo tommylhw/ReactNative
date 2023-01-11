8da31a92476fbeb830cdbdfb55efd0d9c011e3c4
Tommy Wong <lokhim128@gmail.com>
2023-01-11 18:43:33 +0800

    modified:   config.js 	new file:   metro.config.js 	modified:   package-lock.json 	modified:   package.json

--- config.js
+++ config.js
@@ -1,10 +1,25 @@
-import { firebase, getDatabase } from "@react-native-firebase/database";
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
 
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC8ek0UC5JI6HsFvLiB2JowHvIUgoGY7Hk",
-  authDomain: "todotracker-708ae.firebaseapp.com",
-  projectId: "todotracker-708ae",
-  storageBucket: "todotracker-708ae.appspot.com",
-  messagingSenderId: "811696424289",
-  appId: "1:811696424289:web:6c597662d856025aafb120"
+  apiKey: 'AIzaSyC8ek0UC5JI6HsFvLiB2JowHvIUgoGY7Hk',
+  authDomain: 'todotracker-708ae.firebaseapp.com',
+  projectId: 'todotracker-708ae',
+  storageBucket: 'todotracker-708ae.appspot.com',
+  messagingSenderId: '811696424289',
+  appId: "1:811696424289:web:6c597662d856025aafb120",
+  databaseURL: 'https://todotracker-708ae.firebaseio.com',
 };
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
