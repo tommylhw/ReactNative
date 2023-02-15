d14f56456333dc209fb4d77b3ac03f522f9d1395
tommylhw <62752366+tommylhw@users.noreply.github.com>
2023-02-15 13:38:36 +0800

    new file:   Database/.DS_Store 	modified:   Database/ProjectManager/App.js

--- Database/ProjectManager/App.js
+++ Database/ProjectManager/App.js
@@ -5,9 +5,6 @@
  * @format
  */
 
-// Testing git
-// Testing two
-
 import React, { useState, useEffect } from 'react';
 
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
